ec3e9029ad3353159ea688c90b03eb30254ccb23
Sujata-Metapercept <127724939+Sujata-Metapercept@users.noreply.github.com>
2023-09-29 18:54:06 +0530

    Add files via upload
     Client_Admin Edit User

--- Client_Admin_Edit.cy.js
+++ Client_Admin_Edit.cy.js
@@ -1,5 +1,9 @@
 import { loginApplication } from "../ReusableComponents/LoginApplication.cy";
 import { WaitFunction } from "../ReusableComponents/Function.cy";
+import {
+  updatef_username,
+  update_githubEmail,
+} from "../ReusableComponents/Variable.cy";
 
 describe("To Verify Edit Functionality for Users Modules  ", () => {
   it("Test Scenario 1] To Check  User should able to Edit all filled Except Github Username  ", () => {
@@ -30,16 +34,49 @@ describe("To Verify Edit Functionality for Users Modules  ", () => {
     // cy.xpath('(//input[@type="text"])[2]').each(($field) => {
     //   // Check if the field has a value
     //   cy.wrap($field).should("have.value");
-
     // });
-    cy.get("input#updatef_username.form-control").should(
-      "have.value",
-      "vedantika"
-    );
-    cy.xpath('//input[@id="updatef_username"]')
 
+    // cy.get("form-control").invoke("val").as("username");
+    var FormUsername = cy
+      .get("input#updatef_username.form-control")
+      .invoke("val")
+      .as("username");
+    WaitFunction();
+    cy.xpath('//input[@id="updatef_username"]').clear().type(updatef_username);
+    WaitFunction;
+    // cy.xpath('//button[@type="submit"]').click({ force: true });
+    // WaitFunction();
+    // WaitFunction();
+
+    // cy.xpath('(//td[@aria-colindex="1"])[1]').contains(updatef_username);
+    // WaitFunction();
+
+    // var FormProject = cy
+    //   .get("multiselect__select")
+    //   .invoke("val")
+    //   .as("username");
+
+    // cy.xpath('//div[@class="multiselect__select"]').click({ force: true });
+    // WaitFunction();
+
+    var FormEmailAddress = cy
+      .get("input#updatef_userrole_BV_option_3.custom-control-input")
+      .invoke("val")
+      .as("userrole");
+    cy.xpath('(//label[@class="custom-control-label"])[4]').click({
+      force: true,
+    });
+    WaitFunction();
+    WaitFunction();
+
+    var FormEmailAddress = cy
+      .get("input#updatef_githubEmail.form-control")
+      .invoke("val")
+      .as("githubEmail");
+    cy.xpath('//input[@name="updatef_githubEmail"]')
       .clear()
-      .type("Vedantikaa");
-    cy.xpath('//button[@type="submit"]').click({ force: true });
+      .type(update_githubEmail);
+    WaitFunction();
+    WaitFunction();
   });
 });
